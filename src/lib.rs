436ac249b436e0e103dea6378de12ae15321cb7b
Guoli Lyu <guoli-lv@hotmail.com>
2018-09-06 11:05:12 +0800

    chore: initilization

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! A intuitive rust client for [arangoDB](https://www.arangodb.com/),
+//! inspired by [pyArango](https://github.com/tariqdaouda/pyArango).
+//!
+
+pub mod collection;
+pub mod connection;
+pub mod database;
+pub mod document;
+pub mod query;
+pub mod users;
