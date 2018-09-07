88fb11c7fe6c69b463229f03af2a4a59c7684a0c
Guoli Lyu <guoli-lv@hotmail.com>
2018-09-07 16:55:51 +0800

    feat: basic methods for collections

---
+++ src/collection.rs
@@ -0,0 +1,120 @@
+use failure::Error;
+use std::rc::Rc;
+
+use reqwest::{Client, Url};
+use serde::de::{Deserialize, Deserializer, Error as DeError};
+use serde_derive::Deserialize;
+
+use super::database::Database;
+
+#[derive(Debug)]
+pub struct Collection {
+    id: String,
+    name: String,
+    collection_type: CollectionType,
+    base_url: Url,
+    session: Rc<Client>,
+}
+impl<'a, 'b: 'a> Collection {
+    pub fn new<T: Into<String>>(
+        database: &'b Database,
+        name: T,
+        id: T,
+        collection_type: CollectionType,
+    ) -> Result<Collection, Error> {
+        let url = database.get_url().join("collection")?;
+        Ok(Collection {
+            name: name.into(),
+            id: id.into(),
+            session: database.get_session(),
+            base_url: url,
+            collection_type,
+        })
+    }
+    pub fn from_response(
+        database: &'b Database,
+        collection: &CollectionResponse,
+    ) -> Result<Collection, Error> {
+        let url = database.get_url().join("collection")?;
+        Ok(Collection {
+            id: collection.id.to_owned(),
+            name: collection.name.to_owned(),
+            collection_type: collection.collection_type.clone(),
+            session: database.get_session(),
+            base_url: url,
+        })
+    }
+    pub fn get_collection_type(&self) -> &CollectionType {
+        &self.collection_type
+    }
+    pub fn get_id(&self) -> &str {
+        self.id.as_str()
+    }
+    pub fn get_name(&self) -> &str {
+        self.name.as_str()
+    }
+
+    pub fn get_url(&self) -> &Url {
+        &self.base_url
+    }
+
+    pub fn get_session(&self) -> Rc<Client> {
+        Rc::clone(&self.session)
+    }
+}
+
+#[derive(Deserialize)]
+pub struct CollectionResponse {
+    pub id: String,
+    pub name: String,
+    pub status: CollectionStatus,
+    #[serde(rename = "type")]
+    pub collection_type: CollectionType,
+    #[serde(rename = "type")]
+    pub is_system: bool,
+}
+
+pub enum CollectionStatus {
+    NewBorn,
+    Unloaded,
+    Loaded,
+    BeingUnload,
+    Deleted,
+    Loading,
+}
+impl<'de> Deserialize<'de> for CollectionStatus {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = u8::deserialize(deserializer)?;
+        match value {
+            1 => Ok(CollectionStatus::NewBorn),
+            2 => Ok(CollectionStatus::Unloaded),
+            3 => Ok(CollectionStatus::Loaded),
+            4 => Ok(CollectionStatus::BeingUnload),
+            5 => Ok(CollectionStatus::Deleted),
+            6 => Ok(CollectionStatus::Loading),
+            _ => Err(DeError::custom("Undefined behavior. If the crate breaks after a upgrade of ArangoDB, please contact the author.")),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum CollectionType {
+    Document,
+    Edge,
+}
+impl<'de> Deserialize<'de> for CollectionType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = u8::deserialize(deserializer)?;
+        match value {
+            2 => Ok(CollectionType::Document),
+            3 => Ok(CollectionType::Edge),
+            _ => Err(DeError::custom("Undefined behavior. If the crate breaks after a upgrade of ArangoDB, please contact the author.")),
+        }
+    }
+}
--- src/database.rs
+++ src/database.rs
@@ -1,25 +1,72 @@
 use failure::Error;
+use std::collections::HashMap;
 use std::rc::Rc;
 
 use reqwest::{Client, Url};
+use serde_derive::Deserialize;
 
+use super::collection::{Collection, CollectionResponse};
 use super::connection::Connection;
+use super::response::{serialize_response, Response};
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Database {
     name: String,
     base_url: Url,
     session: Rc<Client>,
+    collections: HashMap<String, Collection>,
+    system_collections: HashMap<String, Collection>,
 }
 impl<'a, 'b: 'a> Database {
     pub fn new<T: Into<String>>(conn: &'b Connection, name: T) -> Result<Database, Error> {
         let name = name.into();
         let path = format!("/_db/{}/_api", name.as_str());
-        let url = Url::parse(conn.get_url().as_str())?.join(path.as_str())?;
-        Ok(Database {
+        let url = conn.get_url().join(path.as_str())?;
+        let mut database = Database {
             name,
             session: conn.get_session(),
             base_url: url,
-        })
+            collections: HashMap::new(),
+            system_collections: HashMap::new(),
+        };
+        database.retrieve_collections()?;
+        Ok(database)
+    }
+    /// The last steps of connection establishment is to query the accessible
+    /// databases and cache them in a hashmap of `Databases` objects.
+    ///
+    /// 1. retrieve the names of all the accessible databases
+    /// 1. for each databases, construct a `Database` object and store them in
+    /// `self.databases` for later use
+    ///
+    /// This function uses the API that is used to retrieve a list of
+    /// all databases the current user can access.
+    fn retrieve_collections(&mut self) -> Result<&mut Database, Error> {
+        // an invalid arango_url should never running through initialization
+        // so we assume arango_url is a valid url
+        // When we pass an invalid path, it should panic to eliminate the bug
+        // in development.
+        let url = self.base_url.join("collection").unwrap();
+        let resp = self.session.get(url).send()?;
+        let result: Vec<CollectionResponse> = serialize_response(resp)?;
+        for coll in result.iter() {
+            let collection = Collection::from_response(self, coll)?;
+            if coll.is_system {
+                self.system_collections
+                    .insert(coll.name.to_owned(), collection);
+            } else {
+                self.system_collections
+                    .insert(coll.name.to_owned(), collection);
+            }
+        }
+        Ok(self)
+    }
+
+    pub fn get_url(&self) -> &Url {
+        &self.base_url
+    }
+
+    pub fn get_session(&self) -> Rc<Client> {
+        Rc::clone(&self.session)
     }
 }
