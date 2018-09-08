0ae3001a5d53f39297c011185012305c051bfcbb
Guoli Lyu <guoli-lv@hotmail.com>
2018-09-08 08:49:58 +0800

    tests: unit test on connections

---
+++ src/connection/tests.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+
+use log::{debug, info};
+use pretty_assertions::{assert_eq, assert_ne};
+use reqwest::{header::Basic, Url};
+
+use super::Connection;
+
+const URL: &str = "http://localhost:8529";
+
+#[test]
+fn test_setup() {
+    let _ = pretty_env_logger::try_init();
+}
+#[test]
+fn test_basic_auth() {
+    // let _ = pretty_env_logger::try_init();
+    let conn = Connection::establish_basic_auth(URL, "root", "KWNngteTps7XjrNv").unwrap();
+    let session = conn.get_session();
+    let headers = session.get(URL).build().unwrap().headers();
+    // let basic = headers.get::<Basic>().unwrap();
+}
+
+#[test]
+fn test_jwt_auth() {
+    // let _ = pretty_env_logger::try_init();
+    let conn: Connection = Default::default();
+    let jwt = conn.jwt_login("root", "KWNngteTps7XjrNv").unwrap();
+    info!("JWT login success. Token: {}", jwt);
+    // assert_eq!(false, true);
+}
