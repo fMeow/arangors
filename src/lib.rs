1a44ee25a47e89a16a73f4e2151b69519f73a71d
Guoli Lyu <guoli-lv@hotmail.com>
2018-09-08 15:30:28 +0800

    feat(lib): export useful struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,21 @@
 //! inspired by [pyArango](https://github.com/tariqdaouda/pyArango).
 //!
 
+pub mod aql;
 pub mod collection;
 pub mod connection;
 pub mod database;
+mod query;
 pub mod response;
+
+pub use crate::collection::Collection;
+pub use crate::connection::Connection;
+pub use crate::database::Database;
+
+pub use crate::response::Error;
+pub use crate::response::Query;
+pub use crate::response::Response;
+pub use crate::response::Success;
+
+pub use crate::aql::AqlOption;
+pub use crate::aql::AqlQuery;
