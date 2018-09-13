ebbb39b61b4a617dfb2850793e337730a9d98250
Guoli Lyu <guoli-lv@hotmail.com>
2018-09-13 16:54:14 +0800

    feat(examples): aql example

---
+++ examples/aql.rs
@@ -0,0 +1,19 @@
+use arangors::{AqlQuery, Connection, Cursor, Database};
+use serde_json::value::Value;
+const URL: &str = "http://localhost:8529";
+
+fn main() {
+    pretty_env_logger::init();
+
+    let conn = Connection::establish_jwt(URL, "root", "KWNngteTps7XjrNv").unwrap();
+    let databases = conn.list_all_database().unwrap();
+    println!("{:?}", databases);
+
+    let database = conn.get_database("test_db").unwrap();
+    let aql = AqlQuery::new("FOR u IN test_collection LIMIT 3 RETURN u");
+    println!("{:?}", aql);
+    println!("{:?}", serde_json::to_string(&aql).unwrap());
+
+    let resp: Vec<Value> = database.aql_query(aql).unwrap();
+    println!("{:?}", resp);
+}
