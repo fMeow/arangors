437f203ef14726485754ec0066bd1faafb264eed
Guoli Lyu <guoli-lv@hotmail.com>
2018-09-13 16:38:50 +0800

    doc: use README.md for crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,5 @@
-//! `Arangors` is an intuitive rust client for [arangoDB](https://www.arangodb.com/),
-//! inspired by [pyArango](https://github.com/tariqdaouda/pyArango).
-//!
-//! Arangors enables you to connect with arangoDB server, access to database,
-//! collection and documents in an easy and intuitive way.
-//!
-//! ## NOTICE
-//! `Arangors` is targeted at `Rust 2018`, so this driver would be remains in
-//! nightly channel until the release of `Rust 2018`.
-//!
-//! Also, `arangors` will stay synchronous until the `futures ` crate reach
-//! `1.0`.
-//!
-//!
-//! ## Connection
-//! There is three way to establish connections:
-//! - jwt
-//! - basic auth
-//! - no authentication
-//!
-//! So are the `arangors` API:
-//! Example:
-//!
-//! ```rust,ignore
-//! use arangors::Connection;
-//!
-//! // (Recommended) Handy functions
-//! let conn = Connection::establish_jwt("http://localhost:8529", "username", "password").unwrap();
-//! let conn =
-//!     Connection::establish_basic_auth("http://localhost:8529", "username", "password").unwrap();
-//! let conn = Connection::establish_without_auth("http://localhost:8529", "username", "password")
-//!     .unwrap();
-//! ```
-//!
-//! ## AQL Query
-//! Example:
-//!
-//! ```rust,ignore
-//! use
-//! use arangors::{AqlQuery, Connection, Cursor, Database};
-//! use serde_json::value::Value;
-//!
-//! fn main() {
-//!     pretty_env_logger::init();
-//!
-//!     let conn = Connection::establish_jwt("http://localhost:8529", "username", "password").unwrap();
-//!     let database = conn.get_database("database").unwrap();
-//!
-//!     let aql: AqlQuery<Value> = AqlQuery {
-//!         query: "FOR u IN Collection LIMIT 3 RETURN u",
-//!         batch_size: Some(1),
-//!         ..Default::default()
-//!     };
-//!     let resp: Vec<Value> = database.aql_query(aql).unwrap();
-//!     println!("{:?}", resp);
-//! }
-//! ```
+#![feature(external_doc)]
+#![doc(include = "../README.md")]
 
 pub mod aql;
 pub mod collection;
@@ -63,6 +8,7 @@ pub mod database;
 pub mod document;
 mod query;
 pub mod response;
+mod session;
 
 pub use crate::collection::Collection;
 pub use crate::connection::Connection;
