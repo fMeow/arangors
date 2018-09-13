fd358adf5c1923eb8537dc5f042a050c58f8c168
Guoli Lyu <guoli-lv@hotmail.com>
2018-09-13 16:38:21 +0800

    feat(document): serialize and deserialize trait, initialization

--- src/document.rs
+++ src/document.rs
@@ -1,11 +1,28 @@
+use reqwest::Client;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Document<T> {
+    #[serde(skip_serializing_if = "String::is_empty")]
     _id: String,
+
+    #[serde(skip_serializing_if = "String::is_empty")]
     _key: String,
+
+    #[serde(skip_serializing_if = "String::is_empty")]
     _rev: String,
 
     #[serde(flatten)]
     pub document: T,
+}
+
+impl<T> Document<T> {
+    pub fn new(data: T) -> Self {
+        Document {
+            document: data,
+            _id: String::new(),
+            _key: String::new(),
+            _rev: String::new(),
+        }
+    }
 }
