664423fbdf9c17f99d8ea31d15dd1e15353d0d9e
Guoli Lyu <guoli-lv@hotmail.com>
2018-10-25 18:06:22 +0800

    feat(connection): addition of data model in connection

---
+++ src/connection/model.rs
@@ -0,0 +1,17 @@
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+pub struct Version {
+    pub server: String,
+    pub version: String,
+    pub license: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DatabaseInfo {
+    pub name: String,
+    pub id: String,
+    pub path: String,
+    pub is_system: bool,
+}
