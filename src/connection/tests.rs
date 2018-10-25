b62d4c989ea02b79279cab308472d411405680c7
Guoli Lyu <guoli-lv@hotmail.com>
2018-10-25 17:58:54 +0800

    test(connection): remove basic auth and implement jwt auth test

--- src/connection/tests.rs
+++ src/connection/tests.rs
@@ -2,23 +2,17 @@ use std::collections::HashMap;
 
 use log::{debug, info};
 use pretty_assertions::{assert_eq, assert_ne};
-use reqwest::Url;
+use reqwest::{Url, header::AUTHORIZATION};
 
 use super::Connection;
 
 const URL: &str = "http://localhost:8529";
+const USERNAME: &str = "root";
+const PASSWORD: &str = "KWNngteTps7XjrNv";
 
 #[test]
 fn test_setup() {
     env_logger::init();
-}
-#[test]
-fn test_basic_auth() {
-    // let _ = pretty_env_logger::try_init();
-    let conn = Connection::establish_basic_auth(URL, "root", "KWNngteTps7XjrNv").unwrap();
-    let session = conn.get_session();
-    let headers = session.get(URL).build().unwrap().headers();
-    // let basic = headers.get::<Basic>().unwrap();
 }
 
 #[test]
@@ -27,5 +21,6 @@ fn test_jwt_auth() {
     let conn: Connection = Default::default();
     let jwt = conn.jwt_login("root", "KWNngteTps7XjrNv").unwrap();
     info!("JWT login success. Token: {}", jwt);
-    // assert_eq!(false, true);
+    let not_empty = jwt.len() > 1;
+    assert_eq!(not_empty, true);
 }
