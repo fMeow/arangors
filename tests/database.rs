f2419df57f2f3f28c10b9cb15a5ef5ae60501ea9
Guoli Lyu <guoli-lv@hotmail.com>
2018-10-25 17:57:06 +0800

    test(database): add test for create and drop database

---
+++ tests/database.rs
@@ -0,0 +1,20 @@
+use pretty_assertions::{assert_eq, assert_ne};
+use serde_json::value::Value;
+
+use arangors::{AqlQuery, Connection, Cursor, Database};
+
+const URL: &str = "http://localhost:8529/";
+
+#[test]
+fn setup() {
+    env_logger::init();
+}
+
+#[test]
+fn test_create_and_drop_database() {
+    let conn = Connection::establish_jwt(URL, "root", "KWNngteTps7XjrNv").unwrap();
+    let result = conn.create_database("example").unwrap();
+    assert_eq!(result, true);
+    let result = conn.drop_database("example").unwrap();
+    assert_eq!(result, true);
+}
