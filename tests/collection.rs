b165b3732739b010b6055ac831edfb7e97f6157a
arn-the-long-beard <arn.menant@gmail.com>
2020-06-18 16:28:51 +0200

    test: added unit test for collection::document_count

--- tests/collection.rs
+++ tests/collection.rs
@@ -4,8 +4,9 @@
 use log::trace;
 use pretty_assertions::assert_eq;
 
-use arangors::{ClientError, Connection};
+use arangors::{ClientError, Connection, Document};
 use common::{get_arangodb_host, get_normal_password, get_normal_user, test_setup};
+use serde_json::Value;
 
 pub mod common;
 
@@ -102,6 +103,61 @@ async fn test_get_properties() {
     assert_eq!(result.key_options.last_value, Some(0));
     assert_eq!(result.status, 3);
     assert_eq!(result.write_concern, 1);
+
+    let coll = database.drop_collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+}
+
+#[maybe_async::test(
+    sync = r#"any(feature="reqwest_blocking")"#,
+    async = r#"any(feature="reqwest_async")"#,
+    test = "tokio::test"
+)]
+#[cfg_attr(feature = "surf_async", maybe_async::must_be_async, async_std::test)]
+async fn test_get_document_count() {
+    test_setup();
+    let host = get_arangodb_host();
+    let user = get_normal_user();
+    let password = get_normal_password();
+
+    let collection_name = "test_collection_count";
+
+    let conn = Connection::establish_jwt(&host, &user, &password)
+        .await
+        .unwrap();
+    let mut database = conn.db("test_db").await.unwrap();
+
+    let coll = database.drop_collection(collection_name).await;
+    assert_eq!(coll.is_err(), true);
+
+    let coll = database.create_collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+
+    let coll = database.collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+    let coll = coll.unwrap();
+    let count = coll.document_count().await;
+
+    let result = count.unwrap();
+    assert_eq!(result.count, Some(0));
+    assert_eq!(result.name, collection_name);
+    assert_eq!(result.cache_enabled, false);
+    assert_eq!(result.is_system, false);
+    assert_eq!(result.wait_for_sync, false);
+    assert_eq!(result.key_options.allow_user_keys, true);
+    assert_eq!(result.key_options.r#type, Some("traditional".to_string()));
+    assert_eq!(result.key_options.last_value, Some(0));
+    assert_eq!(result.status, 3);
+    assert_eq!(result.write_concern, 1);
+
+    let query: Vec<Value> = database
+        .aql_str(r#"INSERT {  "name": "test_user" } INTO test_collection_count"#)
+        .await
+        .unwrap();
+
+    let updated_count = coll.document_count().await;
+    let updated_result = updated_count.unwrap();
+    assert_eq!(updated_result.count, Some(1));
 
     let coll = database.drop_collection(collection_name).await;
     assert_eq!(coll.is_err(), false);
