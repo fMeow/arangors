17d3b6cefe7123f252612fe1d01e67887acf36d8
arn-the-long-beard <arn.menant@gmail.com>
2020-06-24 10:08:40 +0200

    feat: implemented collection::recalculate_count & unit test

--- tests/collection.rs
+++ tests/collection.rs
@@ -563,3 +563,40 @@ async fn test_put_rename() {
     let coll = database.drop_collection(new_name).await;
     assert_eq!(coll.is_err(), false);
 }
+
+#[maybe_async::test(
+    sync = r#"any(feature="reqwest_blocking")"#,
+    async = r#"any(feature="reqwest_async")"#,
+    test = "tokio::test"
+)]
+#[cfg_attr(feature = "surf_async", maybe_async::must_be_async, async_std::test)]
+async fn test_put_recalculate() {
+    test_setup();
+    let host = get_arangodb_host();
+    let user = get_normal_user();
+    let password = get_normal_password();
+
+    let collection_name = "test_collection_recalculate";
+
+    let conn = Connection::establish_jwt(&host, &user, &password)
+        .await
+        .unwrap();
+    let mut database = conn.db("test_db").await.unwrap();
+
+    let coll = database.drop_collection(collection_name).await;
+    assert_eq!(coll.is_err(), true);
+
+    let coll = database.create_collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+
+    let coll = database.collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+    let coll = coll.unwrap();
+    let recalculate = coll.recalculate_count().await;
+
+    let result = recalculate.unwrap();
+    assert_eq!(result.result, true);
+
+    let coll = database.drop_collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+}
