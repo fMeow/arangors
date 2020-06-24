7ecb25e134dff4feebd193f3b5982b6affbdd88f
arn-the-long-beard <arn.menant@gmail.com>
2020-06-24 11:08:41 +0200

    feat: implemented collection::rotate_journal & unit test but need check

--- tests/collection.rs
+++ tests/collection.rs
@@ -600,3 +600,41 @@ async fn test_put_recalculate() {
     let coll = database.drop_collection(collection_name).await;
     assert_eq!(coll.is_err(), false);
 }
+
+#[cfg(any(feature = "mmfiles"))]
+#[maybe_async::test(
+    sync = r#"any(feature="reqwest_blocking")"#,
+    async = r#"any(feature="reqwest_async")"#,
+    test = "tokio::test"
+)]
+#[cfg_attr(feature = "surf_async", maybe_async::must_be_async, async_std::test)]
+async fn test_put_rotate_journal() {
+    test_setup();
+    let host = get_arangodb_host();
+    let user = get_normal_user();
+    let password = get_normal_password();
+
+    let collection_name = "test_collection_rotate_journal";
+
+    let conn = Connection::establish_jwt(&host, &user, &password)
+        .await
+        .unwrap();
+    let mut database = conn.db("test_db").await.unwrap();
+
+    let coll = database.drop_collection(collection_name).await;
+    assert_eq!(coll.is_err(), true);
+
+    let coll = database.create_collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+
+    let coll = database.collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+    let coll = coll.unwrap();
+    let rotate = coll.rotate_journal().await;
+
+    let result = rotate.unwrap();
+    assert_eq!(result.result, true);
+
+    let coll = database.drop_collection(collection_name).await;
+    assert_eq!(coll.is_err(), false);
+}
