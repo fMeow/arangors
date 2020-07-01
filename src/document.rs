d5e1805753df53133a0c1830a2fe3b977c408ba6
arn-the-long-beard <arn.menant@gmail.com>
2020-07-01 11:32:50 +0200

    feat: updated DocumentInsertOptions & Document impl & added DocumentHeader & Document Response

--- src/document.rs
+++ src/document.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
+use std::fmt::Debug;
 
 /// Options for document insertion.
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
 pub struct DocumentInsertOptions {
     /// Wait until document has been synced to disk.
     pub wait_for_sync: Option<bool>,
@@ -15,9 +15,26 @@ pub struct DocumentInsertOptions {
     /// No meta-data will be returned for the created document.
     /// This option can be used to save some network traffic.
     pub silent: Option<bool>,
-    /// If set to true, the insert becomes a replace-insert.
-    /// If a document with the same _key already exists the new document is not rejected with unique constraint violated but will replace the old document.
-    pub overwrite: Option<bool>,
+    // If set to true, the insert becomes a replace-insert.
+    // If a document with the same _key already exists the new document is not rejected with unique constraint violated but will replace the old document.
+    //  pub overwrite: Option<DocumentOverwriteMode>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DocumentHeader {
+    pub _id: String,
+    pub _key: String,
+    pub _rev: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DocumentResponse<T> {
+    #[serde(flatten)]
+    pub header: Option<DocumentHeader>,
+    pub new: Option<Document<T>>,
+    #[serde(rename = "_oldRev")]
+    pub _old_red: Option<String>,
+    pub old: Option<Document<T>>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -35,7 +52,10 @@ pub struct Document<T> {
     pub document: T,
 }
 
-impl<T> Document<T> {
+impl<'de, T> Document<T>
+where
+    T: Serialize + Deserialize<'de> + Debug,
+{
     pub fn new(data: T) -> Self {
         Document {
             document: data,
