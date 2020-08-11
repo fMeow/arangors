fb0b60e72a48d3b6911ac6bd81d2ef71599d3bec
arn-the-long-beard <arn.menant@gmail.com>
2020-08-11 12:57:03 +0200

    doc: added example for Aql::bind_var with builder pattern

--- examples/aql.rs
+++ examples/aql.rs
@@ -6,11 +6,19 @@
 
 use std::io::Write;
 
+use serde::{Deserialize, Serialize};
 use serde_json::value::Value;
 
 use arangors::{AqlQuery, Connection};
 
 const URL: &str = "http://localhost:8529";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    first_name: String,
+    last_name: String,
+    email: String,
+}
 
 // See this example when you want an async code
 #[cfg(any(feature = "reqwest_async", feature = "surf_async"))]
@@ -32,6 +40,22 @@ async fn main() {
 
     let resp: Vec<Value> = database.aql_query(aql).await.unwrap();
     println!("{:?}", resp);
+
+    let collection = "test_collection";
+    let user = User {
+        first_name: "John".to_string(),
+        last_name: "Doe".to_string(),
+        email: "john.doe@who.com".to_string(),
+    };
+    let json = serde_json::to_value(&user).unwrap();
+    let aql = AqlQuery::builder()
+        .query("INSERT @user INTO @@collection LET result = NEW RETURN result")
+        .bind_var("@collection", collection)
+        .bind_var("user", json)
+        .build();
+
+    let result: Vec<User> = database.aql_query(aql).await.unwrap();
+    println!("{:?}", result)
 }
 
 // See this example when you want an blocking code
